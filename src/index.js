27ba2fcd1a4ffa5c9265e484d6e4486f35a94cf5
sanditya2510 <sanditay2510@gmail.com>
2020-12-20 15:38:56 +0530

    [fix] - changed title

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import rootReducer from './reducers/root'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 
-import '@fortawesome/fontawesome-free/css/all.min.css'; import
-'bootstrap-css-only/css/bootstrap.min.css'; import
-'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
